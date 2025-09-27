ac2aec25d79440f937563aefe0adbd0a53f0e80e
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-09-27 14:22:18 +0000

    controler user

---
+++ PostgreSQLAtv/api/controller/user.js
@@ -0,0 +1,27 @@
+const getAllUsers = (req, res) =>{
+
+}
+
+const getUserById = (req, res) =>{
+
+}
+
+const creteUser = (req, res) =>{
+
+}
+
+const updateUserById = (req, res) => {
+
+}
+
+const deleteUserById = (req,res) => {
+
+}
+
+export {
+    getAllUsers,
+    getUserById,
+    creteUser,
+    updateUserById,
+    deleteUserById
+}
