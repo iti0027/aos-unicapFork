1691665174e5ad89793b59d3d0586794d5041397
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-09-27 14:11:03 +0000

    criando o message router

--- PostgreSQLAtv/api/routes/message.js
+++ PostgreSQLAtv/api/routes/message.js
@@ -1,36 +1,119 @@
 import { v4 as uuidv4 } from "uuid";
 import { Router } from "express";
+import models from '../models/index.js';
+
+const Message = models.Message;
+const User = models.User;
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
+router.get("/", async (req, res) => {
+    try {
+      const messages = await Message.findAll();
+      return res.send({messages: messages});
+    } catch (error) {
+      return res.status(500).send({
+        message: "Erro interno do servidor ao buscar mensagens",
+      });
+    }
 });
 
-router.get("/:messageId", (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
+router.get("/:messageId", async (req, res) => {
+    try {
+      const {messageId} = req.params;
+      const message = await Message.findByPk(messageId);
+      if(!message){
+        return res.status(404).send({
+          message: "Messagem não encontrada.",
+        });
+      }
+      return res.status(200).send(message);
+    }catch (error){
+      return res.status(500).send({
+        message: "Erro interno do servidor so buscar mensagem."
+      });
+    }
 });
 
-router.post("/", (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.me.id,
-  };
+router.post('/', async (req, res) => {
+    try {
+        const { userId } = req.query;
+        const { text } = req.body;
 
-  req.context.models.messages[id] = message;
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return res.status(404).send({
+                message: 'Usuário não existente com o id fornecido',
+            });
+        }
 
-  return res.send(message);
+        const newMessage = await Message.create({
+            text: text,
+            userId: userId,
+        });
+        return res.status(201).send({
+            message: 'Mensagem criada com sucesso',
+            mensagem: newMessage,
+        });
+    } catch (error) {
+        return res.status(500).send({
+            message: 'Erro interno do servidor ao criar mensagem',
+        });
+    }
 });
 
-router.delete("/:messageId", (req, res) => {
-  const { [req.params.messageId]: message, ...otherMessages } =
-    req.context.models.messages;
+router.put('/:messageId', async (req, res) => {
+    try {
+        const { messageId } = req.params;
+        const { text } = req.body;
 
-  req.context.models.messages = otherMessages;
+        const message = await Message.findByPk(messageId);
+        if (!message) {
+            return res.status(404).send({
+                message: 'Mensagem não encontrada',
+            });
+        }
 
-  return res.send(message);
+        await Message.update(
+            { text: text },
+            {
+                where: { id: messageId },
+            }
+        );
+
+        const updatedMessage = await Message.findByPk(messageId);
+        return res.status(200).send({
+            message: 'Mensagem atualizada com sucesso',
+            mensagem: updatedMessage,
+        });
+    } catch (error) {
+        return res.status(500).send({
+            message: 'Erro interno do servidor ao atualizar mensagem',
+        });
+    }
+});
+
+router.delete('/:messageId', async (req, res) => {
+    try {
+        const { messageId } = req.params;
+        const message = await Message.findByPk(messageId);
+        if (!message) {
+            return res.status(404).send({
+                message: 'Mensagem não encontrada',
+            });
+        }
+
+        await Message.destroy({
+            where: { id: messageId },
+        });
+        return res.status(204).send({
+            message: 'Mensagem deletada com sucesso',
+        });
+    } catch (error) {
+        return res.status(500).send({
+            message: 'Erro interno do servidor ao deletar mensagem',
+        });
+    }
 });
 
 export default router;
