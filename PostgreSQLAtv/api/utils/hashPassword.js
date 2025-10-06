114c35d24b92d4fe459b5d94954e7caf4d40d110
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-10-06 22:42:32 +0000

    criando a função hashPassword

---
+++ PostgreSQLAtv/api/utils/hashPassword.js
@@ -0,0 +1,16 @@
+import bcrypt from bcrypt;
+
+const hashPassword = async (password) =>{
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+}
+
+const verifyPassword = async (password, hashedPassword) => {
+    return await bcrypt.compare(password, hashedPassword);
+}
+
+export{
+    hashPassword,
+    verifyPassword
+}
